4e0a294db56bb879e4e598fbd425ed386679b51c
mcaravind <blogsooner@gmail.com>
2015-03-04 00:25:00 -0800

    Basic implementation of sentence tokenization

---
+++ NLTKSharp/SentenceTokenizer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLTKSharp
+{
+    public static class SentenceTokenizer
+    {
+        static string[] knownAbbreviations = new string[]{"etc.","mr.","mrs."};
+        public static List<string> Tokenize(string input)
+        {
+            List<string> result = new List<string>();
+            StringBuilder sb = new StringBuilder();
+            int lastSpaceIndex = Int16.MaxValue;
+            for (int i = 0; i < input.Length; i++)
+            {
+                char ch = input[i];
+                if (ch == ' ')
+                {
+                    lastSpaceIndex = i;
+                }
+                sb.Append(ch);
+                int nextNonWhitespaceCharDist = input.Substring(i + 1).TakeWhile(c => (char.IsWhiteSpace(c)||c=='\n'||c=='\r')).Count();
+                if (nextNonWhitespaceCharDist > 2)
+                {
+                    //mark as EOS
+                    i = i + nextNonWhitespaceCharDist;
+                    result.Add(sb.ToString());
+                    sb = new StringBuilder();
+                }
+                else
+                {
+                    if ((ch == '.'||ch==':'||ch=='?'||ch=='!') && i < input.Length - 1)
+                    {
+                        StringBuilder sbWord = new StringBuilder();
+                        for (int k = lastSpaceIndex + 1; k <= i; k++)
+                        {
+                            sbWord.Append(input[k]);
+                        }
+                        string currWord = sbWord.ToString();
+                        if (knownAbbreviations.Any(x => sb.ToString().EndsWith(x)))
+                        {
+                            //just an abbreviation, move to next
+                        }
+                        else if (currWord.Length <= 4)
+                        {
+                            int n;
+                            bool isNumeric = int.TryParse(currWord, out n);
+                            if (!isNumeric)
+                            {
+                                //word is small, probably an abbreviation
+                                if (currWord.All(c => char.IsUpper(c)))
+                                {
+                                    //all uppercase, very likely an abbreviation
+                                }
+                                else
+                                {
+                                    //possible eos
+                                    result.Add(sb.ToString());
+                                    sb = new StringBuilder();
+                                }
+                            }
+                        }
+                        else
+                        {
+                            char nextChar = input[i + 1];
+                            char nextNonWhitespaceChar = input[i + nextNonWhitespaceCharDist+1];
+                            if (char.IsUpper(nextNonWhitespaceChar) || nextChar == '\r' || nextChar == '\n')
+                            {
+                                //next character is uppercase
+                                //likely eos
+                                result.Add(sb.ToString());
+                                sb = new StringBuilder();
+                            }
+                        }
+                    }
+
+                }
+            }
+            result.Add(sb.ToString());
+            return result;
+        } 
+    }
+}
--- StemmerTest/UnitTest1.cs
+++ StemmerTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NLTKSharp;
 
@@ -7,8 +8,17 @@ namespace StemmerTest
     [TestClass]
     public class UnitTest1
     {
-        
 
+        [TestMethod]
+        public void TestSentenceTokenization()
+        {
+            string text = @"It's easy to understand for developers and 'plays well with everything'.
+Don't underestimate the importance of toolchain support. Want to spin up a Ruby microservice which speaks HTTP? Sinatra and you're done. Go? Whatever the Go HTTP library is and you're done. Need to interact with it from the command line? Curl and you're done. How about from an automated testing script written in Ruby? Net:HTTP/HTTParty and you're done. Thinking about how to deploy it vis-a-vis firewall/etc? Port 80 and you're done. Need coarse-grained access logging? Built into Nginx/etc already; you're done. Uptime monitoring? Expose a /monitor endpoint; provide URL to existing monitoring solution; you're done. Deployment orchestration? Use Capistrano/shell scripts/whatever you already use for the app proper and you're done. Encrypted transport? HTTPS and you're done. Auth/auth? A few options that you're very well-acquainted with and are known to be consumable on both ends of the service trivially.
+Edit to note: I'm assuming, in the above, that one is already sold on the benefits of a microservice architecture for one's particular app/team and is deciding on transport layer for the architecture. FWIW, I run ~4 distinct applications, and most of them are comparatively large monolithic Rails apps. My company's bookkeeping runs in the same memory space as bingo card PDF generation.
+Things that would tilt me more towards microservices include a very rapid deployment pace, large engineering organizations which multiple distinct teams which each want to be able to control deploys/architecture decisions, particular hotspots in the application which just don't play well with one's main technology stack (as apparently happened in the app featured in this article), etc.";
+            List<string> sentences = SentenceTokenizer.Tokenize(text);
+            Assert.AreEqual(sentences.Count>2,true);
+        }
         [TestMethod]
         public void TestStemming()
         {
